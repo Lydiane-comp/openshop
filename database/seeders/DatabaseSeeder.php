d56dbcd05c4c2f686c82c0b6d7d914fed84c6cd8
Abdoul-Aziz ZOROM <abdoulazizzorom@gmail.com>
2021-04-27 14:57:59 +0000

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             // ProduitSeeder::class,
             // CategorySeeder::class,
+            RoleSeeder::class,
         ]);
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
     }
 }
